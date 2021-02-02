9f39e8c8d3031e9f6a71106013cb66a583ed9c60
Utkarsh Dixit <utkarshdixit19@gmail.com>
2021-02-03 00:34:31 +0530

    Change error message

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.post("/",function(req,res){
   }
 
   const request = https.request(url,options,function(response){
+
       if(response.statusCode===200) {
         res.sendFile(__dirname+"/success.html");
 
@@ -55,7 +56,7 @@ app.post("/",function(req,res){
       console.log(JSON.parse(data));
     })
   })
-  //request.write(jsonData);
+  request.write(jsonData);
   request.end();
 
 });
@@ -63,7 +64,7 @@ app.post("/",function(req,res){
 app.post("/failure",function(req,res){
   res.redirect("/");
 })
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT ||3000,function(){
   console.log("Server is running on port 3000");
 
 })
